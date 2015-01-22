6c1681805acb4ccbf168299615e82fa3fbf0113b
MarkDunning <mark.dunning@gmail.com>
2015-01-22 14:15:29 +0000

    create new scripts to get specific types of data

---
+++ get-exome-data.sh
@@ -0,0 +1,4 @@
+mkdir -p /resources/exome/
+cd /resources/exome/
+wget -O NA12878-NGv3-LAB1360-A_1.fastq.gz https://s3.amazonaws.com/bcbio_nextgen/NA12878-NGv3-LAB1360-A_1.fastq.gz
+wget -O NA12878-NGv3-LAB1360-A_2.fastq.gz https://s3.amazonaws.com/bcbio_nextgen/NA12878-NGv3-LAB1360-A_2.fastq.gz
---
+++ get-rna-seq.sh
@@ -0,0 +1,2 @@
+mkdir /resources/rna-seq
+wget http://bioinf.wehi.edu.au/RNAseqCaseStudy/data.tar.gz -P /resources/rna-seq
