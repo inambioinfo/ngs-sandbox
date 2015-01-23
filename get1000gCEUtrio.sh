821d05c3d5cabe8e17b015834c1c870445ce3c60
MarkDunning <mark.dunning@gmail.com>
2015-01-23 11:28:35 +0000

    scripts to get pasilla and 1000genomes data

---
+++ get1000gCEUtrio.sh
@@ -0,0 +1,10 @@
+##Get data for a CEU trio
+## https://github.com/tk2/RetroSeq/wiki/1000-Genome-CEU-Trio-Analysis
+
+mkdir -P /resources/test-data/1000gCEUtrio/
+wget ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/technical/working/20120117_ceu_trio_b37_decoy/CEUTrio.HiSeq.WEx.b37_decoy.NA12878.clean.dedup.recal.20120117.bam -P /resources/test-data/1000gCEUtrio/
+  wget ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/technical/working/20120117_ceu_trio_b37_decoy/CEUTrio.HiSeq.WEx.b37_decoy.NA12878.clean.dedup.recal.20120117.bam.bai  -P /resources/test-data/1000gCEUtrio/  
+wget ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/technical/working/20120117_ceu_trio_b37_decoy/CEUTrio.HiSeq.WEx.b37_decoy.NA12891.clean.dedup.recal.20120117.bam -P /resources/test-data/1000gCEUtrio/
+  wget ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/technical/working/20120117_ceu_trio_b37_decoy/CEUTrio.HiSeq.WEx.b37_decoy.NA12891.clean.dedup.recal.20120117.bam.bai -P /resources/test-data/1000gCEUtrio/
+wget ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/technical/working/20120117_ceu_trio_b37_decoy/CEUTrio.HiSeq.WEx.b37_decoy.NA12892.clean.dedup.recal.20120117.bam -P /resources/test-data/1000gCEUtrio/
+  wget ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/technical/working/20120117_ceu_trio_b37_decoy/CEUTrio.HiSeq.WEx.b37_decoy.NA12892.clean.dedup.recal.20120117.bam.bai -P /resources/test-data/1000gCEUtrio/
---
+++ getPasillaBams.sh
@@ -0,0 +1,26 @@
+  mkdir -p /resources/test-data/pasilla
+  wget http://www-huber.embl.de/pub/DEXSeq/analysis/brooksetal/bam/treated1.bam -P /resources/test-data/pasilla
+  wget http://www-huber.embl.de/pub/DEXSeq/analysis/brooksetal/bam/treated1.bam.bai -P /resources/test-data/pasilla
+
+  wget http://www-huber.embl.de/pub/DEXSeq/analysis/brooksetal/bam/treated2.bam -P /resources/test-data/pasilla
+  wget http://www-huber.embl.de/pub/DEXSeq/analysis/brooksetal/bam/treated2.bam.bai -P /resources/test-data/pasilla
+
+
+  wget http://www-huber.embl.de/pub/DEXSeq/analysis/brooksetal/bam/treated3.bam -P /resources/test-data/pasilla
+  wget http://www-huber.embl.de/pub/DEXSeq/analysis/brooksetal/bam/treated3.bam.bai -P /resources/test-data/pasilla
+
+  
+  wget http://www-huber.embl.de/pub/DEXSeq/analysis/brooksetal/bam/untreated1.bam -P /resources/test-data/pasilla
+  wget http://www-huber.embl.de/pub/DEXSeq/analysis/brooksetal/bam/untreated1.bam.bai -P /resources/test-data/pasilla
+  
+  wget http://www-huber.embl.de/pub/DEXSeq/analysis/brooksetal/bam/untreated2.bam -P /resources/test-data/pasilla
+  wget http://www-huber.embl.de/pub/DEXSeq/analysis/brooksetal/bam/untreated2.bam.bai -P /resources/test-data/pasilla
+  
+  
+  wget http://www-huber.embl.de/pub/DEXSeq/analysis/brooksetal/bam/untreated3.bam -P /resources/test-data/pasilla
+  wget http://www-huber.embl.de/pub/DEXSeq/analysis/brooksetal/bam/untreated3.bam.bai -P /resources/test-data/pasilla
+  
+  wget http://www-huber.embl.de/pub/DEXSeq/analysis/brooksetal/bam/untreated4.bam -P /resources/test-data/pasilla
+  wget http://www-huber.embl.de/pub/DEXSeq/analysis/brooksetal/bam/untreated4.bam.bai -P /resources/test-data/pasilla
+  
+  wget ftp://ftp.ensembl.org/pub/release-62/gtf/drosophila_melanogaster/Drosophila_melanogaster.BDGP5.25.62.gtf.gz -P /resources/test-data/pasilla
